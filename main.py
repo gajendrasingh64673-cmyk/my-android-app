c46e3a015a60fd7902b9edf7ee4822b26dcc96ed
gajendrasingh64673-cmyk <gajendrasingh64673@gmail.com>
2026-04-05 16:18:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,14 @@
 import kivy
 from kivy.app import App
 # ... (आपका बाकी सारा कोड यहाँ आएगा)
+name: Build APK
+on: [push, pull_request]
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - name: Buildozer Action
+        uses: ArtemSerebriakov/buildozer-action@v1
+        with:
+          command: buildozer android debug
