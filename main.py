e7f243419b40e8d516ed68a328564a190e52366c
gajendrasingh64673-cmyk <gajendrasingh64673@gmail.com>
2026-04-05 16:28:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-
-# यहाँ अपना Pydroid वाला पूरा PDF Scanner कोड पेस्ट करें
-import kivy
-from kivy.app import App
-# ... (आपका बाकी सारा कोड यहाँ आएगा)
 name: Build APK
 on: [push, pull_request]
 jobs:
@@ -11,17 +6,6 @@ jobs:
     steps:
       - uses: actions/checkout@v2
       - name: Buildozer Action
-        uses: ArtemSerebriakov/buildozer-action@v1
-        with:
-          command: buildozer android debug
-name: Build APK
-on: [push, pull_request]
-jobs:
-  build:
-    runs-on: ubuntu-latest
-    steps:
-      - uses: actions/checkout@v2
-      - name: Buildozer Action
-        uses: ArtemSerebriakov/buildozer-action@v1
+        uses: nick-fields/buildozer-action@v1
         with:
           command: buildozer android debug
